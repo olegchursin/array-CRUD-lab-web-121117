7f228c922a03834a82504b5a20809e22dd954305
Oleg Chursin <olegchursin@gmail.com>
2017-11-27 18:39:37 +0000

    automatically backed up by learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -26,11 +26,10 @@ end
 
 def retrieve_element_from_index(array, index_number)
   array.delete_at(index_number)
-
 end
 
 def retrieve_first_element_from_array(array)
-
+ array(0)
 end
 
 def retrieve_last_element_from_array(array)
